27a94dafb03fde5aac83e71d953e177b2e473048
Oussama Rahmouni <ousrah@hotmail.com>
2022-02-14 12:24:21 +0100

    Ajoutez des fichiers projet.

---
+++ Cryptage/frmCriptage.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+
+namespace Cryptage
+{
+    public partial class frmCriptage : Form
+    {
+        public frmCriptage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           textBox2.Text = Convert.ToBase64String(myLib.EncryptSym(textBox1.Text, myLib.cle, myLib.iv));
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+              textBox2.Text = myLib.DecryptSym(System.Convert.FromBase64String(textBox1.Text), myLib.cle, myLib.iv);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CspParameters cspParams = new CspParameters();
+
+            // Les clés publique et privée
+           
+            RSAParameters publicKeys;
+
+            using (var rsa = new RSACryptoServiceProvider(cspParams))
+            {
+                // charger la clé publique
+                rsa.FromXmlString(myLib.pu);
+             
+                publicKeys = rsa.ExportParameters(false);
+                rsa.Clear();
+            }
+
+
+            // La clé publique est utilisée pour chiffrer les données
+            textBox2.Text = Convert.ToBase64String(myLib.EncryptAssym (textBox1.Text, publicKeys));
+          
+      
+
+
+
+
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            myLib.Generate_cle_iv();
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            myLib.generate_public_private_key();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CspParameters cspParams = new CspParameters();
+
+            // La clé privée
+            RSAParameters privateKeys;
+        
+            using (var rsa = new RSACryptoServiceProvider(cspParams))
+            {
+                // charger la clé privée
+                rsa.FromXmlString(myLib.pr);
+                privateKeys = rsa.ExportParameters(true);
+               
+                rsa.Clear();
+            }
+
+
+            // La clé privée est utilisée pour déchiffrer les données
+            textBox2.Text = myLib.DecryptAssym (System.Convert.FromBase64String(textBox1.Text), privateKeys);
+
+
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = myLib.hash(textBox1.Text);
+        }
+    }
+}
---
+++ Cryptage/frmCriptage.designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Cryptage
+{
+    partial class frmCriptage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 14);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(769, 218);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(26, 255);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(769, 213);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1220, 48);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 60);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Crypter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1361, 48);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Décrypter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1220, 191);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 58);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Crypter";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1358, 191);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 58);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Décrypter";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(821, 48);
+            this.button5.Margin = new System.Windows.Forms.Padding(5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(389, 60);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Générer la clé et l\'initialisation";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(825, 192);
+            this.button6.Margin = new System.Windows.Forms.Padding(5);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(387, 57);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Générer la clé publique et privé";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(823, 332);
+            this.button7.Margin = new System.Windows.Forms.Padding(5);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(131, 57);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Hasher";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(818, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(238, 29);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Cryptage Symétrique";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(818, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 29);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Cryptage Asymétrique";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(820, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 29);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Le Hashage";
+            // 
+            // frmCriptage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1534, 482);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "frmCriptage";
+            this.Text = "Le Cryptage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
